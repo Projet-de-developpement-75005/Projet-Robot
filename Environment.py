42b8a63e67016936e35f6ae1c74378cf9e05751b
Diana <dianaaraour123@gmail.com>
2025-02-03 18:28:36 +0100

    suppression de la methode est_valide de la classe environnement

--- Environment.py
+++ Environment.py
@@ -1,13 +1,15 @@
+import pygame
+
+# Couleurs
+BLANC = (255, 255, 255)
+NOIR = (0, 0, 0)
+
 class Environment:
-    def __init__(self, largeur, longueur, obstacles):
+    def __init__(self, largeur, longueur):
         self.largeur = largeur
         self.longueur = longueur
-        self.obstacles = obstacles
+        self.fenetre = pygame.display.set_mode((largeur, hauteur))
+        pygame.display.set_caption("Simulation de Robot 🤖")
 
-    def est_valide_position(self, x, y):
-        """Vérifie si une position est valide (pas d'obstacle, pas hors des limites)"""
-        if x < 0 or x >= self.largeur or y < 0 or y >= self.longueur:
-            return False
-        if (x, y) in self.obstacles:
-            return False
-        return True
+
+    
