b2e2318e2bd5f5d4886a872f071c3def4221329d
Diana <dianaaraour123@gmail.com>
2025-02-03 17:22:51 +0100

    modifier le constructeur de la classe robot en ajoutant des attributs

--- Robot.py
+++ Robot.py
@@ -1,10 +1,23 @@
+import math
+import pygame
+
+# Couleurs
+ROUGE = (200, 0, 0)
+BLEU = (0, 0, 200)
+
+# Paramètres du robot
+ROBOT_LONGUEUR = 60
+ROBOT_LARGEUR = 30
+
 class Robot:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.angle = 0  # Orientation de la voiture
-        self.vitesse_roue_gauche = 0  # Vitesse de la roue gauche
-        self.vitesse_roue_droite = 0  # Vitesse de la roue droite
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.angle = 0  # Orientation du robot
+        self.vitesse_roue_gauche = 0
+        self.vitesse_roue_droite = 0
+
+    
 
 
     def afficher_position(self):
@@ -15,3 +28,5 @@ class Robot:
         """Tourne le robot d'un certain angle (en degrés)"""
         self.orientation = (self.orientation + angle) % 360
         print(f"Orientation actuelle : {self.orientation}°")
+        
+    
