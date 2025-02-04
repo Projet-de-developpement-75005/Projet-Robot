42a5b7e5a793d4aefa244c3eda4caad504c0d867
Wissam75005 <arkamwissam44@.gmail.com>
2025-02-04 20:57:09 +0100

    modification de init

--- Env_robot.py
+++ Env_robot.py
@@ -7,8 +7,37 @@ from interface import Interface, VOITURE_LONGUEUR, VOITURE_LARGEUR
 
 
 class EnvRobot:
-    def __init__(self, robot, environment):
-        self.robot = robot
-        self.environment = environment
+    def _init_(self):
+        pygame.init()
+        self.largeur = 900
+        self.hauteur = 800
+        self.environnement = Environnement(self.largeur, self.hauteur)
+        self.voiture = Voiture(self.largeur // 2, self.hauteur // 2)
+        self.interface = Interface(self.largeur, self.hauteur)
+        self.clock = pygame.time.Clock()
+        self.running = True
 
-    
+        # Démarrer l'horloge
+        self.temps_depart = time.time()
+
+        # Demander à l'utilisateur s'il veut entrer manuellement les vitesses et la direction
+        self.entrer_manuellement = input("Voulez-vous entrer manuellement les vitesses et la direction ? (o/n) : ").lower() == "o"
+
+        if self.entrer_manuellement:
+            # Entrer les vitesses des roues
+            self.voiture.vitesse_roue_gauche = int(input("Entrez la vitesse de la roue gauche (-8 à 8) : "))
+            self.voiture.vitesse_roue_droite = int(input("Entrez la vitesse de la roue droite (-8 à 8) : "))
+
+            # Entrer la direction
+            direction = input("Entrez la direction (haut, bas, gauche, droite) : ").lower()
+            if direction == "haut":
+                self.voiture.angle = 90
+            elif direction == "bas":
+                self.voiture.angle = 270
+            elif direction == "gauche":
+                self.voiture.angle = 180
+            elif direction == "droite":
+                self.voiture.angle = 0
+            else:
+                print("Direction non reconnue. Utilisation de la direction par défaut (haut).")
+                self.voiture.angle = 90
