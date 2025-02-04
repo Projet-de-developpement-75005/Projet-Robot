494b96e6bdd54423bb36027989abff72a7c0ba13
Wissam75005 <arkamwissam44@.gmail.com>
2025-02-04 20:44:37 +0100

    import des bibliotheques et des modules necessaire

--- Env_robot.py
+++ Env_robot.py
@@ -1,4 +1,10 @@
+import pygame
+import time
 import math 
+from robot import Voiture
+from environnement import Environnement
+from interface import Interface, VOITURE_LONGUEUR, VOITURE_LARGEUR
+
 
 class EnvRobot:
     def __init__(self, robot, environment):
