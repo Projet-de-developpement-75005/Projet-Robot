09e0c80b8213dbc1d52cbbbd8569fe1f233dec54
Amel <amel.ben.chabane22@gmail.com>
2025-02-12 11:14:17 +0100

    Création du controller et déclaration des méthodes

---
+++ Controller.py
@@ -0,0 +1,14 @@
+import math
+import time  # Importer time pour gérer le temps
+
+class Controller:
+    def _init_(self, robot, environment):
+        self.robot = robot
+        self.environment = environment
+        self.temps_depart = time.time()  # Stocker le temps de départ
+
+def vitesse_roues(self, left_speed, right_speed):
+def deplacer_robot(self):
+def verif_collision(self):
+def _deplacer_trajectoire_carre(self):
+def _gerer_deplacement_clavier(self):
