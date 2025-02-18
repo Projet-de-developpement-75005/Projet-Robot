926b388278a7925a93569abea86265f09ea9a625
Wissam75005 <arkamwissam44@.gmail.com>
2025-02-18 20:06:54 +0100

    --ff

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from Env_robot import EnvRobot
 
 class Main:
-    def __init__(self):
-        self.simulation = EnvRobot()
+    
 
     def run(self):
         self.simulation.demarrer_simulation()
