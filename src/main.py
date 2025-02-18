99568f61e1e6e76d3c62fbdff4ea79b47667ae62
Amel <amel.ben.chabane22@gmail.com>
2025-02-18 20:05:02 +0100

    Ajout des prints dans le constructeur afin de choisir le mode de déplacement du robot

--- src/main.py
+++ src/main.py
@@ -7,6 +7,13 @@ class Main:
         self.root.title("Simulation Robot")
         self.canvas = tk.Canvas(self.root, width=900, height=800, bg="white")
         self.canvas.pack()
+        print("Choisissez un mode :")
+        print("1 - Mode Carré (le robot se déplace automatiquement en carré)")
+        print("2 - Mode Classique (contrôle au clavier ou avec des vitesses)")
+        choix = input("Entrez 1 ou 2 : ")
+
+        self.simulation = EnvRobot(self.canvas, mode=int(choix))
+
     def run(self):
         self.simulation.demarrer_simulation()
         self.root.mainloop()
