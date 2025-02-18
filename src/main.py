c7bebcbd7cfa39cc673ef5d2eedac53c9304ec3f
Amel <amel.ben.chabane22@gmail.com>
2025-02-18 19:54:18 +0100

    Ajout de la méthode run dans la classe main

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
+import tkinter as tk
 from Simulation import EnvRobot
 
 class Main:
-    def __init__(self):
-        self.simulation = EnvRobot()
-
+    def _init_(self):
+        self.root = tk.Tk()
+        self.root.title("Simulation Robot")
+        self.canvas = tk.Canvas(self.root, width=900, height=800, bg="white")
+        self.canvas.pack()
     def run(self):
         self.simulation.demarrer_simulation()
+        self.root.mainloop()
 
-if __name__ == "__main__":
-    app = Main()
-    app.run()
+    if name == "main":
+        app = Main()
+        app.run()
