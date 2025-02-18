7e21a01f5f8cc579f6c2ea1337a071f856d6c59f
Wissam75005 <arkamwissam44@.gmail.com>
2025-02-18 19:26:54 +0100

    changement de la classe init(self) du  main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+import tkinter as tk
 from Simulation import EnvRobot
 
 class Main:
-    def __init__(self):
-        self.simulation = EnvRobot()
+    def _init_(self):
+        self.root = tk.Tk()
+        self.root.title("Simulation Robot")
+        self.canvas = tk.Canvas(self.root, width=900, height=800, bg="white")
+        self.canvas.pack()
 
     def run(self):
         self.simulation.demarrer_simulation()
