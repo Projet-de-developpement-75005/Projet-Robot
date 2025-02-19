3378e5d472de7596b1414e3bffe45ffbd66d81e9
Wissam75005 <arkamwissam44@.gmail.com>
2025-02-19 10:24:42 +0100

    suppression des fonctions :demander_controle_utilisateur,deplacer_trajectoire_carre,activer_controle_clavier et set_vitesse

--- src/Simulation.py
+++ src/Simulation.py
@@ -42,37 +42,4 @@ class EnvRobot:
             self.interface.rafraichir_ecran(self.robot, self.obstacles, time.time() - self.temps_depart)
             time.sleep(0.03)
 
-    def demander_controle_utilisateur(self):
-        print("Voulez-vous entrer manuellement les vitesses des roues ? (o/n)")
-        choix = input().lower()
-        if choix == "o":
-            self.robot.vitesse_roue_gauche = float(input("Entrez la vitesse de la roue gauche (-8 à 8) : "))
-            self.robot.vitesse_roue_droite = float(input("Entrez la vitesse de la roue droite (-8 à 8) : "))
-        else:
-            print("Utilisez les touches du clavier pour contrôler la voiture.")
-            self.activer_controle_clavier()
-                
-
-    def _deplacer_trajectoire_carre(self):
-        """Déplace le robot sur une trajectoire en carré tout en vérifiant les limites."""
-        if self.cote_parcouru < self.cote_carre:
-            # Avancer dans la direction actuelle
-            self.robot.vitesse_roue_gauche = self.robot.vitesse_roue_droite = 5  # Vitesse de déplacement
-            self.cote_parcouru += 1
-        else:
-            # Après avoir parcouru un côté, tourner de 90°
-            self.robot.angle += 90
-            self.cote_courant = (self.cote_courant % 4) + 1
-            self.cote_parcouru = 0
-
-    def activer_controle_clavier(self):
-        """Ajoute des touches pour contrôler le robot avec ZQSD (ou flèches)."""
-        self.interface.canvas.bind_all("<Up>", lambda e: self.set_vitesse(3, 3))
-        self.interface.canvas.bind_all("<Down>", lambda e: self.set_vitesse(-3, -3))
-        self.interface.canvas.bind_all("<Left>", lambda e: self.set_vitesse(-2, 2))
-        self.interface.canvas.bind_all("<Right>", lambda e: self.set_vitesse(2, -2))
-        self.interface.canvas.bind_all("<space>", lambda e: self.set_vitesse(0, 0))
-        
-    def set_vitesse(self, gauche, droite):
-        self.robot.vitesse_roue_gauche = gauche
-        self.robot.vitesse_roue_droite = droite
+    
