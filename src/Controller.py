05034cd8e02f1e4facf8330c2ae1f6ba79dd1d0e
Tassadit <tassaditaksas03@gmail.com>
2025-02-19 10:06:58 +0100

    ajout de la methode qui demande le controlle a lutilisateur

--- src/Controller.py
+++ src/Controller.py
@@ -10,6 +10,17 @@ class Controller:
         self.cote_carre = 100  # Nombre de pas pour parcourir un côté du carré
         self.cote_courant = 1  # Côté actuel du carré (1 à 4)
 
+    def demander_controle_utilisateur(self):
+        """Demande à l'utilisateur s'il souhaite entrer manuellement les vitesses des roues."""
+        print("Voulez-vous entrer manuellement les vitesses des roues ? (o/n)")
+        choix = input().lower()
+        if choix == "o":
+            self.robot.vitesse_roue_gauche = float(input("Entrez la vitesse de la roue gauche (-8 à 8) : "))
+            self.robot.vitesse_roue_droite = float(input("Entrez la vitesse de la roue droite (-8 à 8) : "))
+        else:
+            print("Utilisez les touches du clavier pour contrôler la voiture.")
+            self.activer_controle_clavier(self.environnement.interface.canvas)  # Activer le contrôle clavier
+            
 def vitesse_roues(self, left_speed, right_speed):
 def deplacer_robot(self):
 def verif_collision(self):
