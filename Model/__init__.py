6dbde6e4b00106aed484b46b155d41128d6ae153
Diana <dianaaraour123@gmail.com>
2025-03-10 22:53:31 +0100

    import des classes arena, robot, obstacle,et update_model dans le fichier init.py du module Model

---
+++ Model/__init__.py
@@ -0,0 +1,8 @@
+from .arene import Arena
+from .robot import Robot
+from .obstacle import Obstacle
+
+
+
+# Importation des fonctions ou modules liés à la mise à jour du modèle
+from .update_model import update_model
