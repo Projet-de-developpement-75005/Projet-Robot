4379e7b8360151a99051de9dcc246e4d22c7b98c
Melia75005 <thafatm@gmail.com>
2025-03-10 21:33:42 +0100

    Suppression de Mymain.py

--- MyEnvironment.py
+++
@@ -1,9 +0,0 @@
-class Environment:
-    def __init__(self, largeur, hauteur):
-        self.largeur = largeur
-        self.hauteur = hauteur
-        self.obstacles = [
-            (200, 300, 50, 50),  # Obstacle 1: (x, y, largeur, hauteur)
-            (500, 400, 60, 60),  # Obstacle 2
-            (700, 200, 40, 40)   # Obstacle 3
-        ]
