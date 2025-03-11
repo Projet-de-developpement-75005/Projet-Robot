3b60143e58488b0e21f1c53e878de9ef6a37f2a3
Diana <dianaaraour123@gmail.com>
2025-03-11 13:13:37 +0100

    faire sortir le fichier main.py du dossier main et supprimer le dossier main

--- Model/robot.py
+++ Model/robot.py
@@ -5,5 +5,7 @@ class Robot:
         self.y = y
         self.direction = direction  # En degrés : 0 = vers la droite
 
+
+
     def __str__(self):
         return f"Robot(pos=({self.x:.2f}, {self.y:.2f}), dir={self.direction}°)"
