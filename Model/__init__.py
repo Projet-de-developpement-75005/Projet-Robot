2e2973dff5272fc75f525c1d46dd3b96b795e80b
Melia75005 <thafatm@gmail.com>
2025-03-18 17:32:05 +0100

    Modifier le fichier __init__.py

--- Model/__init__.py
+++ Model/__init__.py
@@ -1,2 +1,8 @@
-from .arene import Arena
+from .arene import Arene
 from .robot import Robot
+from .obstacle import Obstacle
+
+
+
+# Importation des fonctions ou modules liés à la mise à jour du modèle
+from .update_model import update_model
