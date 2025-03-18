d619bfdfb782bff171762b5379459e058d31a28b
Amel <amel.ben.chabane22@gmail.com>
2025-03-18 14:12:36 +0100

    Modifier __init__.py dans Model

--- Model/__init__.py
+++ Model/__init__.py
@@ -1,8 +1,2 @@
 from .arene import Arena
 from .robot import Robot
-from .obstacle import Obstacle
-
-
-
-# Importation des fonctions ou modules liés à la mise à jour du modèle
-from .update_model import update_model
