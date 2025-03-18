348779c5c96783c4af031545bc85632cb91734df
Tassadit <tassaditaksas03@gmail.com>
2025-03-18 16:17:11 +0100

    supprimer les méthodes add_objet et str

--- Model/arene.py
+++ Model/arene.py
@@ -1,14 +1,16 @@
-class Arena:
-    def __init__(self, width=400, height=400, scale=1):
-        """Initialise l'arène avec ses dimensions et un éventuel facteur de mise à l'échelle."""
-        self.width = width
-        self.height = height
-        self.scale = scale
-        self.objets = []  # On peut ajouter des obstacles ou autres objets si nécessaire
+class Arene:
+    def _init_(self, largeur, hauteur):
+        self.largeur = largeur
+        self.hauteur = hauteur
+        self.obstacles = []
+        self.robot = None
 
-    def add_object(self, obj):
-        """Ajoute un objet (par exemple, un obstacle) à l'arène."""
-        self.objets.append(obj)
+    
 
-    def __str__(self):
-        return f"Arena({self.width}x{self.height})"
+
+
+
+
+
+    
+
