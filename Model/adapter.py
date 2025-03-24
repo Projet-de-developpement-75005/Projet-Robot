3a5d0ba83e87309276175e7408a820e26e08c667
Diana <dianaaraour123@gmail.com>
2025-03-24 12:37:38 +0100

    ajouter la methode reset-angle qui reinitialise l'angle

--- Model/adapter.py
+++ Model/adapter.py
@@ -23,21 +23,13 @@ class Proxy_Virtuel:
         vitesse_moyenne = (vitesse_gauche_lin + vitesse_droite_lin) / 2
         self._distance += abs(vitesse_moyenne)
         
-    def set_vitesse(self, vitesse_gauche, vitesse_droite):
-        self.robot.set_vitesses(vitesse_gauche, vitesse_droite)
-    
-    def update_distance(self):
-        # On utilise le déplacement linéaire moyen pour simuler la distance parcourue
-        rayon_roue = self.robot.diametre_roue / 2
-        vitesse_gauche_lin = self.robot.vitesse_gauche * rayon_roue
-        vitesse_droite_lin = self.robot.vitesse_droite * rayon_roue
-        vitesse_moyenne = (vitesse_gauche_lin + vitesse_droite_lin) / 2
-        self._distance += abs(vitesse_moyenne)
 
 	def reset_distance(self):
 		self.distance_parcourue = 0
     
-
+    def reset_angle(self):
+		self.angle_parcouru = 0
+		self.angle_depart = self.robot.orientation
 
         
     
