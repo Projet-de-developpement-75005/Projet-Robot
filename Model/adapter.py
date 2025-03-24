edd00cf8bab8594dc409f36eb378e16874e1505c
Tassadit <tassaditaksas03@gmail.com>
2025-03-24 12:48:24 +0100

    ajouter la méthode update_angle qui met a jour l'angle du robot

--- Model/adapter.py
+++ Model/adapter.py
@@ -1,3 +1,6 @@
+import time
+import numpy as np 
+
 class Proxy_Virtuel:
 	def __init__(self, robot, obstacles):
 		self.robot = robot
@@ -26,3 +29,11 @@ class Proxy_Virtuel:
 	def reset_angle(self):
 		self.angle_parcouru = 0
 		self.angle_depart = self.robot.orientation
+
+	def update_angle(self):
+		now = time.time()
+		if self.last_update == 0:
+			self.last_update = now
+		else:
+			ang1, ang2 = self.get_vitAng()
+			self.angle_parcouru += (now - self.last_update) * (ang1 - ang2) * self.rayon / self.dist_roue * 180 / np.pi
