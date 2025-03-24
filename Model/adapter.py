a47f918a024fcd032fdafb76700438468a6c4ee8
Diana <dianaaraour123@gmail.com>
2025-03-24 12:25:19 +0100

    corriger le constructeur et ajout des attributs necessaires pour les autres methodes du fichier

--- Model/adapter.py
+++ Model/adapter.py
@@ -9,5 +9,13 @@ class Proxy_Virtuel:
 
     def set_vitesse(self, vitesse_gauche, vitesse_droite):
         self.robot.set_vitesses(vitesse_gauche, vitesse_droite)
+    
+    def update_distance(self):
+        # On utilise le déplacement linéaire moyen pour simuler la distance parcourue
+        rayon_roue = self.robot.diametre_roue / 2
+        vitesse_gauche_lin = self.robot.vitesse_gauche * rayon_roue
+        vitesse_droite_lin = self.robot.vitesse_droite * rayon_roue
+        vitesse_moyenne = (vitesse_gauche_lin + vitesse_droite_lin) / 2
+        self._distance += abs(vitesse_moyenne)
 
 
