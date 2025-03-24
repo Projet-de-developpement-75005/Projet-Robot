17b1f71620db0e8f1821d8a093fe554274f15e40
Diana <dianaaraour123@gmail.com>
2025-03-24 12:40:28 +0100

    corriger les erreurs d'indentation

--- Model/adapter.py
+++ Model/adapter.py
@@ -1,5 +1,3 @@
-from .robot import Robot
-
 class Proxy_Virtuel:
 	def __init__(self, robot, obstacles):
 		self.robot = robot
@@ -11,28 +9,20 @@ class Proxy_Virtuel:
 		self.angle_parcouru = 0
 		self.last_update = 0
 		self.angle_depart = robot.orientation
-  
-    def set_vitesse(self, vitesse_gauche, vitesse_droite):
-        self.robot.set_vitesses(vitesse_gauche, vitesse_droite)
-    
-    def update_distance(self):
-        # On utilise le déplacement linéaire moyen pour simuler la distance parcourue
-        rayon_roue = self.robot.diametre_roue / 2
-        vitesse_gauche_lin = self.robot.vitesse_gauche * rayon_roue
-        vitesse_droite_lin = self.robot.vitesse_droite * rayon_roue
-        vitesse_moyenne = (vitesse_gauche_lin + vitesse_droite_lin) / 2
-        self._distance += abs(vitesse_moyenne)
-        
+
+	def set_vitesse(self, vitesse_gauche, vitesse_droite):
+		self.robot.set_vitesses(vitesse_gauche, vitesse_droite)
+
+	def update_distance(self):
+		rayon_roue = self.robot.diametre_roue / 2
+		vitesse_gauche_lin = self.robot.vitesse_gauche * rayon_roue
+		vitesse_droite_lin = self.robot.vitesse_droite * rayon_roue
+		vitesse_moyenne = (vitesse_gauche_lin + vitesse_droite_lin) / 2
+		self.distance_parcourue += abs(vitesse_moyenne)
 
 	def reset_distance(self):
 		self.distance_parcourue = 0
-    
-    def reset_angle(self):
+
+	def reset_angle(self):
 		self.angle_parcouru = 0
 		self.angle_depart = self.robot.orientation
-
-        
-    
-
-
-
