9544e946926f59d1960e17edca25773cf1412fe1
Diana <dianaaraour123@gmail.com>
2025-03-24 11:56:12 +0100

    ajouter la methode set-vitesse qui met a jour les vitesses de la roue gauche et droite du robot

--- Model/adapter.py
+++ Model/adapter.py
@@ -7,4 +7,7 @@ class Proxy_Virtuel:
         self._distance = 0
         self._angle = 0
 
+    def set_vitesse(self, vitesse_gauche, vitesse_droite):
+        self.robot.set_vitesses(vitesse_gauche, vitesse_droite)
+
 
