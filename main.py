b8493b15e40862f53fb4c3f1ee5d480427febdb5
Diana <dianaaraour123@gmail.com>
2025-04-01 21:12:29 +0200

    ajout des obstacles dans la fenetre

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Model.robot import Robot
 from controller.controller import Controller, StrategieAvancer, StrategieTourner, StrategieConditionnelle, StrategieSequentielle
 from view.view_2d import View
 from Model.adapter import Proxy_Virtuel, Proxy_Reel
+from Model.obstacle import Obstacle
 
 # Choisissez le mode : "virtuel" pour la simulation graphique ou "reel" pour un robot physique
 mode = "virtuel"  # ou "reel"
@@ -12,7 +13,13 @@ mode = "virtuel"  # ou "reel"
 dt = 0.05
 
 # Création d'un robot (paramètres ajustables)
-robot = Robot(x=100, y=100, orientation=0, vitesse_gauche=0, vitesse_droite=0, diametre_roue=20, distance_roues=40)
+robot = Robot(x=500, y=400, orientation=0, vitesse_gauche=0, vitesse_droite=0, diametre_roue=20, distance_roues=40)
+obstacle = Obstacle(x=200, y=60, largeur=150, hauteur=100)
+obstacle2 = Obstacle(x=400, y=350, largeur=70, hauteur=160)
+
+
+
+
 
 # Instanciation de l'adapter selon le mode choisi
 if mode == "virtuel":
@@ -24,8 +31,10 @@ else:
 
 # Pour le mode virtuel, on crée une arène et une vue graphique
 if mode == "virtuel":
-    arene = Arene(largeur=800, hauteur=600)
+    arene = Arene(largeur=950, hauteur=800)
     arene.ajouter_robot(robot)
+    arene.ajouter_obstacle(obstacle)
+    arene.ajouter_obstacle(obstacle2)
     view = View(arene)
 
 # Liste pour enregistrer la trajectoire du robot (trace du carré)
@@ -35,8 +44,8 @@ trace_points = []
 # Chaque élément correspond à une séquence : avancer d'une distance donnée, puis tourner de 90°.
 liste_strategies = []
 for _ in range(4):
-    strat_avancer = StrategieAvancer(distance=150, vitesse=10)
-    strat_tourner = StrategieTourner(angle_degres=90, vitesse=3)
+    strat_avancer = StrategieAvancer(distance=120, vitesse=50)
+    strat_tourner = StrategieTourner(angle_degres=90, vitesse=10)
     strat_conditionnelle = StrategieConditionnelle(strat_avancer, strat_tourner)
     liste_strategies.append(strat_conditionnelle)
 
@@ -58,6 +67,7 @@ if mode == "virtuel":
         trace_points.append((robot.x, robot.y))
         # Actualisation de l'affichage graphique en passant la trace
         view.update_affichage(robot, trace_points)
+        
         if not finished:
             view.after(int(dt * 1000), update_simulation)
         else:
