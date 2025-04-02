21d9b1cf3bfca025d18e88475529cbefc3d8adc8
Diana <dianaaraour123@gmail.com>
2025-04-02 09:50:25 +0200

    ajouter la methode draw robot pour deessiner le robot en 3d

--- view/view_3d.py
+++ view/view_3d.py
@@ -11,4 +11,25 @@ class View3D:
         self.ax = self.fig.add_subplot(111, projection='3d')
         self.trace = []
 
+    def draw_robot(self):
+        self.ax.clear()
+        self.ax.set_xlim(0, self.arene.largeur)
+        self.ax.set_ylim(0, self.arene.hauteur)
+        self.ax.set_zlim(0, 100)  # Hauteur fictive
+
+        # Dessiner obstacles
+        for obs in self.arene.obstacles:
+            self.ax.bar3d(obs.x, obs.y, 0, obs.largeur, obs.hauteur, 50, color='red', alpha=0.5)
+
+        # Dessiner robot
+        self.ax.scatter(self.robot.x, self.robot.y, 10, color='blue', s=100)
+
+        # Tracer trajectoire
+        if self.trace:
+            xs, ys = zip(*self.trace)
+            zs = [10] * len(xs)
+            self.ax.plot(xs, ys, zs, color='green')
+
+        plt.draw()
+        plt.pause(0.01)
 
