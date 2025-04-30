8eca5ae7079a502b5cc39e9212b23d4be0999e3f
Diana <dianaaraour123@gmail.com>
2025-04-30 12:20:16 +0200

    modiification du ficheir arene.py(fusion des ficheirs arene.py et obstacle.Py)

--- Model/arene.py
+++ Model/arene.py
@@ -1,41 +1,58 @@
 import math
 
+class Obstacle:
+    def __init_(self,x,y,larg,longueur):
+        self.x=x
+        self.y=y
+        self.larg=larg
+        self.longueur=longueur
+        
+    def collision(self,rx,ry,rayon):
+        """detecte si le robot touche un certain obtacke"""
+        cx = self.x + self.larg/2
+        cy = self.y + self.longueur/2
+        
+        dx=abs(rx-cx)- self.larg/2
+        dy=abs(ry-cy)- self.longueur/2
+        
+        dx2=max(dx,0)
+        dy2=max(dy,0)
+        
+        return (dx **2 +dy **2)<= rayon **2
+    
 class Arene:
-    def __init__(self, largeur, hauteur):
-        self.largeur = largeur
-        self.hauteur = hauteur
-        self.obstacles = []
-        self.robot = None
-
-    def ajouter_robot(self, robot):
-        self.robot = robot
-
-    def ajouter_obstacle(self, obstacle):
-        self.obstacles.append(obstacle)
-
-    def mise_a_jour(self, delta_t):
-        if self.robot:
-            # Calcul de la vitesse linéaire moyenne et du changement d'orientation
-            average_speed = (self.robot.vitesse_gauche + self.robot.vitesse_droite) / 2.0
-            delta_orientation = (self.robot.vitesse_droite - self.robot.vitesse_gauche) / self.robot.distance_roues
-
-            # Calcul de la nouvelle position en fonction de l'orientation actuelle
-            nouvelle_x = self.robot.x + average_speed * delta_t * math.cos(self.robot.orientation)
-            nouvelle_y = self.robot.y + average_speed * delta_t * math.sin(self.robot.orientation)
-            nouvelle_orientation = self.robot.orientation + delta_orientation * delta_t
-
-            # Vérification des collisions pour la nouvelle position
-            collision = False
-            for obstacle in self.obstacles:
-                if obstacle.est_en_collision(nouvelle_x, nouvelle_y, self.robot.rayon):
-                    print("Collision détectée ! Le robot doit s'arrêter.")
-                    collision = True
-                    break
-
-            if not collision:
-                self.robot.x = nouvelle_x
-                self.robot.y = nouvelle_y
-                self.robot.orientation = nouvelle_orientation
-            else:
-                self.robot.vitesse_gauche = 0
-                self.robot.vitesse_droite = 0
+    def __init__(self,largeur, hauteur):
+        self.largeur=largeur
+        self.hauteur=hauteur
+        self.liste_obstacles=[]
+        self.robot_actuel=None
+        
+    
+    def placer_robot(self,robot):
+        self.robot_actuel=robot
+        
+    def ajout_obstacles(self, obstacle):
+        self.liste_obstacles.append(obstacle)
+        
+    def mouvement(self,dt):
+        if self.robot_actuel:
+            vitesse_moyenne=(self.robot_actuel.vitesse_d + self.robot_actuel.vitesse_g)/ 2.0
+            changement_angle=(self.robot_actuel.vitesse_d -self.robot_actuel.vitesse_g) / self.robot_actuel.ecart_roue
+            
+            new_x=self.robot_actuel.pos_x +vitesse_moyenne*dt*math.cos(self.robot_actuel.angle_orientaion)
+            new_y=self.robot_actuel.pos_y +vitesse_moyenne*dt*math.sin(self.robot_actuel.angle_orientaion)
+            
+            new_angle=self.robot_actuel.angle_orientaion+changement_angle
+            
+            i=0
+            while i<len(self.liste_obstacles) and not self.liste_obstacles[i].collision(new_x,new_y,self.robot.rayon):
+                i+=1
+                if i<len(self.liste_obstacles):
+                    print("collision evitée !")
+                    self.robot.vitesse_g=self.vitesse_d=0
+                else:
+                    self.robot.pos_x, self.robot.pos_y=new_x,  new_y
+                    self.robot.angle_orientaion += (self.robot.vitesse_d-self.robot.vitesse_g) /self.robot.ecart_roue
+            
+            
+            
