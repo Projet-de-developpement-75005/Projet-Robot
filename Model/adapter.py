09a35ec1ea33a43d4c04e5b7b0781c81a919fc3e
Melia75005 <thafatm@gmail.com>
2025-05-05 10:42:57 +0200

    Modifier la fonction dist_parcourue

--- Model/adapter.py
+++ Model/adapter.py
@@ -1,10 +1,10 @@
-import time
-from time import pi
+import math
+import time 
 
-class adapterVirtuel:
+class AdapterVirtuel:
     def __init__(self, robot, obstacles):
         self.robot = robot
-        self.obstcales = obstcales 
+        self.obstacles = obstacles
         self.angle = 0
         self.distance = 0
         self.derniere_maj= time.time()
@@ -15,7 +15,7 @@ class adapterVirtuel:
             self.rayon_roue= robot.taille_roue /2
             
     
-    def difinir_vitesse(self,v_gauche, v_droite):
+    def definir_vitesse(self,v_gauche, v_droite):
         if self.robot:
             self.robot.set_vitesses(v_gauche,v_droite)
         self.derniere_maj=time.time()
@@ -29,23 +29,19 @@ class adapterVirtuel:
     def update(self):
         self.last_update = time.time()
         
-    def avancer(self):
-        dt=self.get_dt()
-        self.robot.avancer(dt)
+    def avancer(self,dt):
+        self.robot.deplacement(dt)
         self.update()
         
     def tourner(self):
         dt=self.get_dt()
-        self.robot.tourner(dt)
+        self.robot.rotation(dt)
         self.update()
     
-    def dist_parcourue(self):
-        dt=self.get_dt()
-        vit_moyenne=(self.robot.vitesse_g+self.robot.vitesse_d)/2
-        dist=vit_moyenne * dt
-        self.distance+=dist
-        return self.distance
     
+    def dist_parcourue(self):
+        return self.robot.get_distance()
+
     def get_ang_parcourue(self):
         dt=self.get_dt()
         dt_vitesse=self.robot.vitesse_d-self.robot.vitesse_g
