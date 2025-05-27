6965cf811e5f39a803b8ab379ca99af7c45fcc2a
Amel <amel.ben.chabane22@gmail.com>
2025-05-27 11:10:29 +0200

    ajout de la fonction gerer_collision

--- Model/arene.py
+++ Model/arene.py
@@ -18,7 +18,7 @@ class Obstacle:
         dx2=max(dx,0)
         dy2=max(dy,0)
         
-        return (dx **2 +dy **2)<= rayon **2
+        return (dx2 **2 +dy2 **2)<= rayon **2
     
 class Arene:
     def __init__(self,largeur, hauteur):
@@ -34,25 +34,24 @@ class Arene:
     def ajout_obstacles(self, obstacle):
         self.liste_obstacles.append(obstacle)
         
-    def mouvement(self,dt):
-        if self.robot_actuel:
-            vitesse_moyenne=(self.robot_actuel.vitesse_d + self.robot_actuel.vitesse_g)/ 2.0
-            changement_angle=(self.robot_actuel.vitesse_d -self.robot_actuel.vitesse_g) / self.robot_actuel.ecart_roue
-            
-            new_x=self.robot_actuel.pos_x +vitesse_moyenne*dt*math.cos(self.robot_actuel.angle_orientaion)
-            new_y=self.robot_actuel.pos_y +vitesse_moyenne*dt*math.sin(self.robot_actuel.angle_orientaion)
-            
-            new_angle=self.robot_actuel.angle_orientaion+changement_angle
-            
-            i=0
-            while i<len(self.liste_obstacles) and not self.liste_obstacles[i].collision(new_x,new_y,self.robot.rayon):
-                i+=1
-                if i<len(self.liste_obstacles):
-                    print("collision evitée !")
-                    self.robot.vitesse_g=self.vitesse_d=0
+    def gerer_collisions(self, robot):
+        """
+        Gère les collisions avec les obstacles.
+        """
+        for obstacle in self.liste_obstacles:
+            collision = obstacle.collision(robot.pos_x,robot.pos_y,robot.rayon)
+            if collision:
+                print("Collision détectée!")
+                robot.set_vitesses(0,0)
+                dx = obstacle.x - robot.pos_x
+                dy = obstacle.y - robot.pos_y
+                angle_robot = robot.angle_orientation
+                angle_obstacle = math.atan2(dy, dx)
+                delta_angle = (angle_obstacle - angle_robot + math.pi) % (2 * math.pi) - math.pi
+
+                if -math.pi / 2 <= delta_angle <= math.pi / 2:
+                  robot.set_vitesses(-5,-5)  
                 else:
-                    self.robot.pos_x, self.robot.pos_y=new_x,  new_y
-                    self.robot.angle_orientaion += (self.robot.vitesse_d-self.robot.vitesse_g) /self.robot.ecart_roue
-            
-            
-            
+                    robot.set_vitesses(5,5)
+
+                break
