b74ee23b4aebbc61c8291da618ac6069620cbb20
Diana <dianaaraour123@gmail.com>
2025-05-27 14:07:15 +0200

    creer une nouvelle classe view3d(creation du constructeur)

--- view/view_3d.py
+++ view/view_3d.py
@@ -1,50 +1,39 @@
-# view/view_3d.py
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import time
+from ursina import *
+import math
+
+hauteur_o=10
+hauteur_r=2
 
 class View3D:
-    def __init__(self, arene, robot):
-        self.arene = arene
-        self.robot = robot
-        self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(111, projection='3d')
-        self.trace = []
-
-    def draw_robot(self):
-        self.ax.clear()
-        self.ax.set_xlim(0, self.arene.largeur)
-        self.ax.set_ylim(0, self.arene.hauteur)
-        self.ax.set_zlim(0, 100)  # Hauteur fictive
-
-        # Dessiner obstacles
-        for obs in self.arene.obstacles:
-            self.ax.bar3d(obs.x, obs.y, 0, obs.largeur, obs.hauteur, 50, color='red', alpha=0.5)
-
-        # Dessiner robot
-        self.ax.scatter(self.robot.x, self.robot.y, 10, color='blue', s=100)
-
-        # Tracer trajectoire
-        if self.trace:
-            xs, ys = zip(*self.trace)
-            zs = [10] * len(xs)
-            self.ax.plot(xs, ys, zs, color='green')
-
-        plt.draw()
-        plt.pause(0.01)
-
-    def run_simulation(self, controller, strategie, dt):
-        strategie.start(controller.adapter)
-    
-        while not strategie.update(controller.adapter, dt):
-            if not plt.fignum_exists(self.fig.number):
-                print("Fenêtre fermée par l'utilisateur.")
-                break  # Sort de la boucle si la fenêtre a été fermée
-
-            self.trace.append((self.robot.x, self.robot.y))
-            self.draw_robot()
-            time.sleep(dt)
-
-        strategie.stop(controller.adapter)
-        print("Stratégie terminée.")
-        plt.close(self.fig)  # Ferme proprement la figure s'il reste quelque chose
+    def __init__(self,arene,robot):
+        #inituialiser l'application ursina
+        self.app=Ursina()
+        self.arene=arene
+        self.robot=robot
+        
+        window.title="Simulation 3D Robot"
+        window.borderless=False
+        window.fullscreen=False
+        window.exut_botton.visible=True
+        window.fps_compteur=True #activer l'affichage du nombre de fps
+        window.color=color.gray
+        
+        largeur=arene.largeur
+        hauteur=arene.hauteur
+        
+        self.sol=Entity(model='plane',texture='white_cube',scale=(largeur,1,hauteur),position(largeur/2,0,hauteur/2),texture_scale=(largeur/10,hauteur/10),color=color.green)
+        self.robot=Entity(model='cube',color=color.azure,scale=(robot.ecart_roue,hauteur_r,robot.taille_roue),position=(robot.pos_x,hauteur_r/2,robot.pos_y))
+        self.obstacle_entities =[]
+        for obs in arene.liste_obstacles:
+            obj=Entity(model='cube',
+                       color=color.purple
+                       scale=(obs.larg,hauteur_o,obs.longueur),
+                        position=(obs.x+obs.larg/2,hauteur_o/2,obs.y+obs.longueur/2))
+            self.obstacle_entities.append(obj)
+            
+        self.camera=EditorCamera(position=(largeur/2,50,hauteur/2))
+        #pour cacher les elements d'interface par defaut d'Ursina
+        camera.ui.enabled=False
+        
+        salf.label=Text(text='',origin=(0,18),background=True)
+        
