4fb042179372a1986c0263a56bb436715a8cbf22
Tassadit <tassaditaksas03@gmail.com>
2025-05-27 10:30:24 +0200

    readapter le main en fonction de la nouvelle version de la stratégie conditionnelle

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 from Model.robot import Robot
 from Model.arene import Arene,Obstacle
 from Model.adapter import AdapterVirtuel
-from controller.controller import Controller, StrategieSequentielle, StrategieAvance, StrategieTourner
+from controller.controller import Controller, StrategieSequentielle, StrategieAvance, StrategieTourner, StrategieConditionnelle
 from view.view_2d import View
 
 robot = Robot(pos_x=300, pos_y=300, angle_orientation=0, vitesse_g=0, vitesse_d=0, taille_roue=10, ecart_roue=40)
 arene = Arene(largeur=600, hauteur=600)
-obstacle1=Obstacle(x=300,y=200,larg=50,longueur=50)
+obstacle1=Obstacle(x=350,y=200,larg=50,longueur=50)
 obstacle2=Obstacle(x=100,y=40,larg=50,longueur=50)
 arene.ajout_obstacles(obstacle1)
 arene.ajout_obstacles(obstacle2)
 adapter = AdapterVirtuel(robot, arene.liste_obstacles)
 
-# Stratégie de dessin d'un carré
+# Exemple : si le robot a parcouru moins de 1m, on avance 50 ; sinon on tourne 90°
+# (au lancement le robot n'a rien parcouru donc il avancera)
+def condition_initiale(adapter):
+    return adapter.dist_parcourue() < 100
+
+# On veut voir visuellement la différence entre les deux choix
+conditionnelle = StrategieConditionnelle(
+    condition_fonction=condition_initiale,
+    action1=StrategieAvance(distance_cible=50, vitesse=10),
+    action2=StrategieTourner(angle_deg=90, vitesse_rotation=10)
+)
+
+# Enchaînement après la condition (un carré classique)
 strategie = StrategieSequentielle([
-    StrategieAvance(distance_cible=100, vitesse=50),
-    StrategieTourner(angle_deg=90, vitesse_rotation=30),
-    StrategieAvance(distance_cible=100, vitesse=50),
-    StrategieTourner(angle_deg=90, vitesse_rotation=30),
-    StrategieAvance(distance_cible=100, vitesse=50),
-    StrategieTourner(angle_deg=90, vitesse_rotation=30),
-    StrategieAvance(distance_cible=100, vitesse=50),
-    StrategieTourner(angle_deg=90, vitesse_rotation=30)
+    conditionnelle,
+    StrategieAvance(100, 10),
+    StrategieTourner(90, 10),
+    StrategieAvance(100, 10),
+    StrategieTourner(90, 10),
+    StrategieAvance(100, 10),
+    StrategieTourner(90, 10),
+    StrategieAvance(100, 10),
+    StrategieTourner(90, 10)
 ])
 
 affichage = input("Voulez-vous activer l'affichage graphique ? (oui/non): ").strip().lower()
